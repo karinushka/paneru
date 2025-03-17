dbb355d2a4f00261a0eadce07c795c9ccfe5cea5
karinushka <karinushka@github>
2025-03-17 19:44:08 +0100

    Update the build.rs script to auto-rebuild the zig library.

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
     let base = "/Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk";
@@ -10,11 +11,23 @@ fn main() {
     let hit = format!("{base}/System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks");
     println!("cargo:rustc-link-search=framework={hit}");
 
-    let zigout = env::var("CARGO_MANIFEST_DIR")
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR")
         .map(PathBuf::from)
-        .unwrap_or_default()
-        .join("zig-out/lib");
-    let libprivate = zigout.to_string_lossy();
-    println!("cargo:rustc-link-search={libprivate}");
-    println!("cargo::rustc-link-arg-bins=-Wl,-rpath,{libprivate}");
+        .unwrap_or_default();
+    let zig_src = manifest_dir.join("src/root.zig");
+    let zig_out = manifest_dir.join("zig-out/lib");
+
+    println!("cargo:rerun-if-changed={}", zig_src.to_string_lossy());
+    let status = Command::new("zig")
+        .args(["build"])
+        .status()
+        .expect("Failed to execute zig build");
+    if !status.success() {
+        panic!("zig build failed");
+    }
+
+    let zig_out = zig_out.to_string_lossy();
+    println!("cargo:rustc-link-search={zig_out}");
+    println!("cargo:rustc-link-lib=static=private");
+    println!("cargo:rustc-link-arg-bins=-Wl,-rpath,{zig_out}");
 }

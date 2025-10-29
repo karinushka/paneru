e0171d1a53c84dffebc75dce81d3f04a9977ed3a
karinushka <karinushka@github>
2025-10-29 21:04:39 +0100

    fix: Temporarily remove AX permission checks.
    There seems to be a bug currently in MacOS, which causes this function
    call to SEGV crash. So it is better to just not check for these - and
    once the main loop starts it will try and show a popup to the user
    asking for permissions.

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,6 @@ embed_plist::embed_info_plist!("../assets/Info.plist");
 use events::{Event, EventHandler, EventSender};
 use platform::PlatformCallbacks;
 use skylight::{SLSGetSpaceManagementMode, SLSMainConnectionID};
-use util::check_ax_privilege;
 
 struct CommandReader {
     events: EventSender,
@@ -220,13 +219,6 @@ fn launch() -> Result<()> {
         return Err(Error::new(
             ErrorKind::Unsupported,
             "Option 'display has separate spaces' disabled.",
-        ));
-    }
-    if !check_ax_privilege() {
-        error!("{}: Accessibility privilege is disabled.", function_name!());
-        return Err(Error::new(
-            ErrorKind::Unsupported,
-            "Accessibility privilege is disabled. Enable it in Settings -> Privacy & Security -> Accessibility.",
         ));
     }
 

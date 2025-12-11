b09e66b5a9d04292a2db4536efeb53895dbb9040
Tubo Shi <tubo@tubo.nz>
2025-12-11 14:01:01 -0600

    Update flake version to 0.3.0

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       pkgs = import nixpkgs { system = "aarch64-darwin"; };
       package = pkgs.rustPlatform.buildRustPackage {
         pname = "paneru";
-        version = "0.2.0";
+        version = "0.3.0";
         src = pkgs.lib.cleanSource ./.;
         postPatch = ''
           substituteInPlace build.rs --replace-fail \

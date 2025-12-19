fa6446ec1bce20572883df352238a197f569bd34
karinushka <karinushka@github>
2025-12-19 16:04:42 +0100

    feat: Add a throttling SystemParam.
    This allows an easy way to throttle the running systems - e.g. execute
    them at most once within the specified duration:
    ```
      if throttle.throttled(Duration::from_millis(1000)) {
          return;
      }
    ```

--- src/params.rs
+++ src/params.rs
@@ -1,6 +1,11 @@
-use bevy::ecs::{
-    query::{With, Without},
-    system::{Query, Res, ResMut, Single, SystemParam},
+use std::time::Duration;
+
+use bevy::{
+    ecs::{
+        query::{With, Without},
+        system::{Local, Query, Res, ResMut, Single, SystemParam},
+    },
+    time::{Time, Timer, TimerMode, Virtual},
 };
 use objc2_core_foundation::CGRect;
 use objc2_core_graphics::CGDirectDisplayID;
@@ -73,6 +78,24 @@ impl Configuration<'_> {
     /// Returns true if Mission Control is active.
     pub fn mission_control_active(&self) -> bool {
         self.mission_control_active.0
+    }
+}
+
+#[derive(SystemParam)]
+pub struct ThrottledSystem<'w, 's> {
+    time: Res<'w, Time<Virtual>>,
+    timer: Local<'s, Timer>,
+}
+
+impl ThrottledSystem<'_, '_> {
+    /// Returns true if the system has already once within the last duration and should be
+    /// throttled.
+    pub fn throttled(&mut self, duration: Duration) -> bool {
+        if self.timer.duration().as_secs() == 0 {
+            *self.timer = Timer::from_seconds(duration.as_secs_f32(), TimerMode::Repeating);
+        }
+        self.timer.tick(self.time.delta());
+        !self.timer.just_finished()
     }
 }
 

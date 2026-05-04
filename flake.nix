4a5a40028e34e8e6445451003554f90406783f16
Presi300 <mail@presi300.com>
2026-05-04 22:53:19 +0300

    nix: Updated cargo.lock and flake.lock. (#208)
    As well as changed flake.nix inputs to the ones, recommended by nix-darwin.
    Co-authored-by: Presi300 <canaryisle1@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };

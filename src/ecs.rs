b172561ae51f0a50ed84fea53f86a0dac7e1a46d
karinushka <karinushka@github>
2026-05-17 12:54:46 +0200

    refactor: Window property access existing WindowProperties.

--- src/ecs.rs
+++ src/ecs.rs
@@ -496,7 +496,7 @@ pub fn setup_bevy_app(sender: EventSender, receiver: Receiver<Event>) -> Result<
 }
 
 struct WindowProperties {
-    pub params: Vec<WindowParams>,
+    params: Vec<WindowParams>,
 }
 
 impl WindowProperties {
@@ -542,5 +542,19 @@ impl WindowProperties {
 
     pub fn width_ratio(&self) -> Option<f64> {
         self.params.iter().find_map(|props| props.width)
+    }
+
+    pub fn vertical_padding(&self) -> i32 {
+        self.params
+            .iter()
+            .find_map(|props| props.vertical_padding)
+            .unwrap_or(0)
+    }
+
+    pub fn horizontal_padding(&self) -> i32 {
+        self.params
+            .iter()
+            .find_map(|props| props.horizontal_padding)
+            .unwrap_or(0)
     }
 }
